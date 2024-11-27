b696b59ab6e6384fba0f3340360598dce840fbb4
LucianoLoyola <lucianonicolasloyola@alu.ing.unlp.edu.ar>
2024-11-27 17:38:40 -0300

    Cambio en ettings

--- ServidorDjango/ServidorWeb/ServidorWeb/settings.py
+++ ServidorDjango/ServidorWeb/ServidorWeb/settings.py
@@ -123,7 +123,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 # settings.py
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
