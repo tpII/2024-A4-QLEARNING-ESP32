67c1e35a930e3c11780b75022ef2541e811d38ea
LucianoLoyola <lucianonicolasloyola@alu.ing.unlp.edu.ar>
2024-12-08 20:34:57 -0300

    Agregado test de encoders

--- Crawler-Q-Learning/test/test_servo.c
+++ Crawler-Q-Learning/test/test_servo.c
@@ -1,5 +1,9 @@
 #include "unity.h"
 #include "servo.h"
+#include "encoder.h"
+
+encoder_t encoder1;
+encoder_t encoder2;
 
 // Define the current_pos array to store servo positions
 extern int current_pos[2];
@@ -56,12 +60,59 @@ void test_set_pos(void) {
     TEST_ASSERT_EQUAL(ELBOW_MID_PULSE, current_pos[1]);
 }
 
+
+//TEST UNITARIOS DE ENCODERS
+void test_example(void) {
+    TEST_ASSERT_EQUAL(1, 1);  // Ejemplo simple
+}
+void test_encoder_init(){
+    encoder_init(&encoder1, ENCODER1_OUT);
+    //encoder_init(&encoder2, ENCODER2_OUT);
+    TEST_ASSERT_EQUAL(0,encoder_get_count(&encoder1));
+    //TEST_ASSERT_EQUAL(0,encoder_get_count(&encoder2));
+}
+
+void test_encoder_get_count(){
+    encoder1.count=5;
+    encoder2.count=5;
+    TEST_ASSERT_EQUAL(5,encoder_get_count(&encoder1));
+    //TEST_ASSERT_EQUAL(5,encoder_get_count(&encoder2));
+}
+
+void test_encoder_reset_count(){
+    encoder_reset_count(&encoder1);
+    //encoder_reset_count(&encoder2);
+    TEST_ASSERT_EQUAL(0,encoder_get_count(&encoder1));
+    //TEST_ASSERT_EQUAL(0,encoder_get_count(&encoder2));
+}
+/*
+void test_calcular_disparidad(){
+    int valor1=3;
+    int valor2=6;
+    int resultado=calcular_disparidad(valor1,valor2);
+    TEST_ASSERT_EQUAL(50,resultado);
+}
+*/
+void test_encoder_get_reward(){
+    encoder1.count=10;
+    float reward=encoder_get_reward(&encoder1);
+    TEST_ASSERT_EQUAL_FLOAT(0.5,reward);
+}
+
+
 int runUnityTests(void) {
     UNITY_BEGIN();
     RUN_TEST(test_set_servo_pulse);
     RUN_TEST(test_process_move_shoulder);
     RUN_TEST(test_process_move_elbow);
     RUN_TEST(test_set_pos);
+    //Tests de encoders
+    RUN_TEST(test_example);
+    RUN_TEST(test_encoder_init);
+    RUN_TEST(test_encoder_get_count);
+    RUN_TEST(test_encoder_reset_count);
+    //RUN_TEST(test_calcular_disparidad);
+    RUN_TEST(test_encoder_get_reward);
     return UNITY_END();
 }
 
